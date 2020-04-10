1b456038332bbbcf9d0bf29e0acdcb41f8f8cdbb
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-10 13:09:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,7 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 修改默认主题
+sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
