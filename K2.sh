3c6b271a7c8ce459088e7e1b238b23304dcf943f
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-11 15:46:46 +0800

    Create K2.sh

---
+++ K2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# 修改机器名称
+sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+echo '添加主题argon_light'
+theme_argon='https://github.com/Leo-Jo-My/luci-theme-argon-light-mod'  # 主题地址
+git clone $theme_argon package/lean/luci-theme-argon_light
+echo 'CONFIG_PACKAGE_luci-theme-argon_light=y' >> .config
