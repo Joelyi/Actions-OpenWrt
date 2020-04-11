952569a9d31116fa168ca040bd0eb42a90dfbef2
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-11 16:17:05 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -12,7 +12,7 @@ sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 # echo '添加主题argon_light'
 # theme_argon_light='https://github.com/Leo-Jo-My/luci-theme-argon-light-mod'  # 主题地址
 # git clone $theme_argon_light package/lean/luci-theme-argon_light
