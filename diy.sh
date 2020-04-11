6af2ff62cab10e4beb10a2715eb9b7971434470d
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-11 13:29:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,5 +8,4 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题
-git clone https://github.com/Leo-Jo-My/luci-theme-argon-light-mod
-sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
