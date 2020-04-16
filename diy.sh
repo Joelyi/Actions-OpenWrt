9f8c9f758920aa63ec5a947f22ade0b7affaffe8
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-16 15:32:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,13 @@
 #=================================================
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 # 修改默认主题
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+
+# 添加软件包
+git clong https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+git clone https://github.com/Leo-Jo-My/luci-theme-argon-light-mod.git package/argon_light
+
+./scripts/feeds update -a
+./scripts/feeds install -a
