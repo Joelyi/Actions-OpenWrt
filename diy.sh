9f093d7bbdb30388c98d2245d8df061b667888eb
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-16 21:27:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,11 +13,7 @@ sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
 
 # 添加软件包
 #ssr-plus
-git clong https://github.com/fw876/helloworld.git package/lean/helloworld
-
-#argon_light主题
-git clone https://github.com/Leo-Jo-My/luci-theme-argon-light-mod.git package/lean/luci-theme-argon-light-mod
+git clong https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 
 ./scripts/feeds update -a
-
 ./scripts/feeds install -a
