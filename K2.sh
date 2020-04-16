505c833bf43fd9b397a5c14bceec6c7b8e112303
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-17 00:07:18 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -12,7 +12,7 @@ sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-# sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 
 # 取消默认bootstrap主题
 sed -i '/\+luci-theme-bootstrap/d' feeds/luci/collections/luci/Makefile
