8397a8d8888c44107095fa43cb18925434435b9a
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-16 15:31:19 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -12,7 +12,7 @@ sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
 
 # 添加软件包
 git clong https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
