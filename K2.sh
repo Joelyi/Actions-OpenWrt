f55c2bc8f4c48c9cecceabf7fd3c140e0a208c74
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-17 00:06:34 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -12,14 +12,26 @@ sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/bootstrap/argon_light/g' feeds/luci/collections/luci/Makefile
+
+# 取消默认bootstrap主题
+sed -i '/\+luci-theme-bootstrap/d' feeds/luci/collections/luci/Makefile
+sed -i '/\+luci-theme-bootstrap/d' package/feeds/luci/luci/Makefile
+sed -i '/CONFIG_PACKAGE_luci-theme-bootstrap=y/d' .config
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 删除原软件包
+rm -rf package/lean/Luci-theme-argon
 
 # 添加软件包
-#ssr-plus
-git clong https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-#argon_light主题
-git clone https://github.com/Leo-Jo-My/luci-theme-argon-light-mod.git package/lean/luci-theme-argon-light-mod
+#ssrplus
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+#theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/Luci-theme-argon
+#passwall
+# git clone https://github.com/Lienol/openwrt-package.git package/lean/lienol
 
+#-更新feeds
 ./scripts/feeds update -a
-
+#-安装feeds
 ./scripts/feeds install -a
