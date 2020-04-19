8a0db0c1c4b497a229fb5dcfb5f38580f5f3fb72
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-19 10:17:32 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -14,6 +14,9 @@ sed -i "s/OpenWrt/PSG1218/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.s
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# 修改默认主题
+# sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+
 # 删除原软件包
 rm -rf package/lean/luci-theme-argon  
 
@@ -30,12 +33,6 @@ sed -i '/\+luci-theme-bootstrap/d' feeds/luci/collections/luci/Makefile
 sed -i '/\+luci-theme-bootstrap/d' package/feeds/luci/luci/Makefile
 sed -i '/CONFIG_PACKAGE_luci-theme-bootstrap=y/d' .config
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改默认主题
-# sed -i "s/bootstrap/argon/g" feeds/luci/modules/luci-base/root/etc/config/luci
-
-# 修改自带默认主题
-# sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 
 #-更新feeds
 ./scripts/feeds update -a
