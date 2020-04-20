27643c8d8713583234a761e73961f0576bd8025c
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-04-20 09:37:17 +0800

    Update K2.sh

--- K2.sh
+++ K2.sh
@@ -9,7 +9,7 @@
 sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
 
 # 修改wifi名称
-sed -i "s/OpenWrt/PSG1218/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/OpenWrt/PSG1218/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
