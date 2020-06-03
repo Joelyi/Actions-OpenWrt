60e8b8ad3c6538d8b8f1e2c7fbc594c5669275ac
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-06-03 23:35:59 +0800

    Update PSG1218.sh

--- PSG1218/PSG1218.sh
+++ PSG1218/PSG1218.sh
@@ -22,9 +22,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 添加软件包
 #ssrplus
-# git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld.git package/lean
 #theme-rosy
-git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy package/lean/luci-theme-rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean
 #passwall
 # git clone https://github.com/Lienol/openwrt-package.git package/lean/lienol
 #luci-app-ssr-pro
