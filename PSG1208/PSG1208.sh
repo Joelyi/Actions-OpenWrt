57f34715f28f14bb866244f1f3ffdc60ff177961
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-06-04 14:07:35 +0800

    Update PSG1208.sh

--- PSG1208/PSG1208.sh
+++ PSG1208/PSG1208.sh
@@ -14,15 +14,15 @@ sed -i "s/OpenWrt/PSG1208/g" package/base-files/files/bin/config_generate
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# 删除原软件包
+# rm -rf package/lean/luci-theme-argon  
+
 # 添加主题
 #theme-rosy
 git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
 
 # 修改默认主题
 sed -i 's/bootstrap/rosy/g' feeds/luci/collections/luci/Makefile
-
-# 删除原软件包
-# rm -rf package/lean/luci-theme-argon  
 
 # 添加软件包
 #ssrplus
