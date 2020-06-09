219e9b50ce6127586fd609a0b4f75bb929ebb424
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-06-09 15:25:49 +0800

    Create PSG1218.sh

---
+++ PSG1218.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# 修改机器名称
+sed -i "s/OpenWrt/PSG1218/g" package/base-files/files/bin/config_generate
+
+# 修改wifi名称
+# sed -i "s/OpenWrt/PSG1218/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 删除原软件包
+# rm -rf package/lean/luci-app-unblockmusic
+# rm -rf package/lean/UnblockNeteaseMusic
+# rm -rf package/lean/UnblockNeteaseMusicGo
+
+# 添加主题
+#theme-rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+
+# 修改默认主题
+sed -i 's/bootstrap/rosy/g' feeds/luci/collections/luci/Makefile
+
+# 添加软件包
+#openwet常用软件包
+# git clone https://github.com/kenzok8/openwrt-packages.git package/lean/diy
+#passwall依赖
+# git clone https://github.com/kenzok8/small.git package/lean/small
+#ssrplus
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+#解除网易云音乐播放限制 [Mini]
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/lean/luci-app-unblockneteasemusic-mini
+
+#-更新feeds
+./scripts/feeds update -a
+#-安装feeds
+./scripts/feeds install -a
