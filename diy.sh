2ed094883cc3b8ee72d0d3d84a5e1dc16eff39ef
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-06-22 19:07:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -20,9 +20,11 @@ sed -i 's/bootstrap/rosy/g' feeds/luci/collections/luci/Makefile
 
 # 添加软件包
 #openwet常用软件包
-git clone https://github.com/kenzok8/openwrt-packages.git package/lean/diy
+# git clone https://github.com/kenzok8/openwrt-packages.git package/lean/diy
 #passwall依赖
-git clone https://github.com/kenzok8/small.git package/lean/small
+# git clone https://github.com/kenzok8/small.git package/lean/small
+#ssrplus
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 
 #-更新feeds
 ./scripts/feeds update -a
