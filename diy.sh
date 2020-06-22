72c0e5fd8b276389fef00ca77c4c0c8e4693e469
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-06-22 19:06:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,13 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
