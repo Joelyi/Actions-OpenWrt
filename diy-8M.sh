42e89459a1702958154b5d1fe98d30c74dd1f464
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-07-12 17:46:43 +0800

    Update diy-8M.sh

--- diy-8M.sh
+++ diy-8M.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/bootstrap/material/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/netgear/g' feeds/luci/collections/luci/Makefile
 
 # 添加软件包
 #ssrplus
