10de2dfbe09ac0bccef154f3cc0e45365939c0d9
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-07-22 11:19:26 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,10 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# 修改内核
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+
 # 删除原软件包
 # rm -rf package/lean/luci-app-unblockmusic
 # rm -rf package/lean/UnblockNeteaseMusic
