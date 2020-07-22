36be9e3066109c2bf14239f3a9d5eeb3e560d984
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-07-22 11:25:54 +0800

    Update diy-8M.sh

--- diy-8M.sh
+++ diy-8M.sh
@@ -9,11 +9,11 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/bootstrap/netgear/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 
 # 添加软件包
 #ssrplus
-git clone https://github.com/Joelyi/helloworld.git package/lean/luci-app-ssr-plus
+# git clone https://github.com/Joelyi/helloworld.git package/lean/luci-app-ssr-plus
 
 #-更新feeds
 ./scripts/feeds update -a
