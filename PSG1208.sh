72ddcc59659d1ac68bba2ea1346d262127a59823
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-09-18 15:54:34 +0800

    Update PSG1208.sh

--- PSG1208.sh
+++ PSG1208.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy.sh
+# Description: OpenWrt DIY script (After Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
