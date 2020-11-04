d603782443c375aa49fa94d7d36849d3e4484b2a
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-04 16:48:11 +0800

    Update newifi-d2-part2.sh

--- newifi-d2-part2.sh
+++ newifi-d2-part2.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 device_name='Newifi3-D2'
 wifi_name='Newifi3'
 lan_ip='192.168.2.1'        # Lan Ip地址
