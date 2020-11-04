f9555b4ba7d6e51caad7d42be70b95e9870f55c6
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-04 16:43:29 +0800

    Create newifi-d2-part1.sh

---
+++ newifi-d2-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*Joelyi\)/\1/' feeds.conf.default
+
+# 添加软件包
+#openwet常用软件包
+git clone https://github.com/kenzok8/openwrt-packages.git package/lean/diy
+#passwall依赖
+git clone https://github.com/kenzok8/small.git package/lean/small
